12e85b766ce9e36fba57c1649b8915baab676e66
Garrett Cullen <Garth619@users.noreply.github.com>
2018-10-14 16:23:12 -0700

    push

--- templates/header.php
+++ templates/header.php
@@ -9,9 +9,52 @@
 	<meta name="description" content=""/>
 	<meta name="keywords" content=""/>
 <style type="text/css">
-* {margin:0px;padding:0px;}
+@import url('https://fonts.googleapis.com/css?family=Oswald:600');
 </style>
 
 </head>
 <body>
-header
+
+<header>
+	
+	<div class="header_left">
+		
+		<a class="logo" href="<?php echo $path;?>">
+			
+			<?php echo file_get_contents("images/logo-final.svg"); ?>
+			
+		</a><!-- logo -->
+		
+	</div><!-- header_left -->
+	
+	<div class="header_right">
+		
+		<div class="contact_icon">
+			
+			<a href="">
+				
+				<?php echo file_get_contents("images/icon-phone.svg"); ?>
+				
+			</a>
+			
+		</div><!-- contact_icon -->
+		
+		<div class="notifications">
+			
+			<span class="profile_initial">g</span><!-- profile_initial -->
+			
+			<span class="notification_number">3</span><!-- notification_number -->
+			
+		</div><!-- notifications -->
+		
+		<div class="menu_wrapper">
+			
+			<span class="menu_bar"></span><!-- menu_bar -->
+			<span class="menu_bar"></span><!-- menu_bar -->
+			<span class="menu_bar"></span><!-- menu_bar -->
+			
+		</div><!-- menu_wrapper -->
+		
+	</div><!-- header_right -->
+	
+</header>
