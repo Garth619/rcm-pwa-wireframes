e04c69ce896c3f98b6f7ba98feee94f6eeec2e8e
Garrett Cullen <Garth619@users.noreply.github.com>
2018-10-31 21:07:00 -0700

    push

--- templates/loyalty-single-deal.php
+++ templates/loyalty-single-deal.php
@@ -8,18 +8,14 @@
 		
 		<div class="inner_section">
 			
+			<span class="share"><?php echo file_get_contents("" . $path . "/images/share-01.svg"); ?></span><!-- share -->
+			
 			<h1 class="section_title">Free Flower</h1><!-- section_title -->
 			
 			<h2 class="sub_header">Redeem By 12.10.18</h2><!-- sub_header -->
-		
-			<div class="single_product_image_wrapper">
-				
-				<?php echo file_get_contents("" . $path . "/images/share-01.svg"); ?>
-			
+
 				<img class="single_product_image" src="<?php echo $path;?>/images/single-test.png"/>
-				
-			</div><!-- single_product_image_wrapper -->
-			
+
 			<span class="redeemed_status">3 / 5 Stamps Redeemed</span><!-- redeemed_status -->
 			
 			<div class="stamps_wrapper">
@@ -31,6 +27,14 @@
 				<div class="stamp"></div><!-- stamp -->
 				
 			</div><!-- stamps_wrapper -->
+			
+			<a class="redeem_button">
+				
+				<span>Redeem</span>
+				
+				<span><?php echo file_get_contents("" . $path ."/images/corner-01.svg"); ?></span>
+				
+			</a><!-- redeem_button -->
 			
 			<div class="loyalty_feed_wrapper">
 				
