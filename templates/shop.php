08b99c718367d77b8c54f556e4317445dcd2c7d7
Garrett Cullen <Garth619@users.noreply.github.com>
2019-02-13 21:06:46 -0800

    push

--- templates/shop.php
+++ templates/shop.php
@@ -11,7 +11,7 @@
 	
 	<section id="shop" class="section_template">
 		
-		<div class="inner_section">
+		
 			
 			<h1 class="section_title">Shop</h1><!-- section_title -->
 			
@@ -49,6 +49,134 @@
 								
 								<div class="product_img_wrapper">
 									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/flower.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
 									<img src="<?php echo $path;?>/images/flower.jpg"/>
 									
 								</div><!-- product_img_wrapper -->
@@ -65,9 +193,334 @@
 						
 					</div><!-- product_row -->
 					
+					
+					<div class="product_row">
+						
+						<div class="product_category_wrapper">
+						
+							<span class="product_category">Edibles</span><!-- product_category -->
+						
+							<span class="see_all">See All 10</span><!-- see_all -->
+						
+						</div><!-- product_category_wrapper -->
+						
+						<div class="product_inner_row">
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/edible.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+						</div><!-- product_inner_row -->
+						
+					</div><!-- product_row -->
+					
+					
+					
+					<div class="product_row">
+						
+						<div class="product_category_wrapper">
+						
+							<span class="product_category">Vaporizers</span><!-- product_category -->
+						
+							<span class="see_all">See All 5</span><!-- see_all -->
+						
+						</div><!-- product_category_wrapper -->
+						
+						<div class="product_inner_row">
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+							<div class="single_product">
+								
+								<div class="product_img_wrapper">
+									
+									<div class="see_more_icon"></div><!-- see_more_icon -->
+									
+									<img src="<?php echo $path;?>/images/vaporizer.jpg"/>
+									
+								</div><!-- product_img_wrapper -->
+								
+								<span class="product_price">$25</span><!-- product_price -->
+								
+								<span class="product_name">Product Name</span><!-- product_name -->
+								
+								<span class="company_name">Company Name</span><!-- company_name -->
+								
+							</div><!-- single_product -->
+							
+						</div><!-- product_inner_row -->
+						
+					</div><!-- product_row -->
+					
+					
+					
 				</div><!-- product_feed -->
 			
-		</div><!-- inner_section -->
+		
 		
 	</section><!-- shop -->
 	
